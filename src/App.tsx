d4a01d4b4186fac2614b530350bb6f07134db195
Ashish Rawat <arawat0429@gmail.com>
2025-09-03 21:39:54 +0530

    nothinng much

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
 import Nav from './Components.tsx/Nav.tsx';
 import Footer from './Components.tsx/Footer.tsx';
 import './App.css';
-
+import { useState } from 'react';
 function App() {
+   const [inputValue, setInputValue] = useState('');
   return (
     <>
       <Nav />
-     <h1 className='head'>All Your Notes Gonna appear here</h1>
+      <div className='header'>
+     <h1>All Your Notes Gonna Appear Here</h1>
+      </div>
+      <div className="app">
+          <input type="text" placeholder='Type your note here...' />
+          <button className='btn' onClick={()=>{ 
+            let input = (document.querySelector('input') as HTMLInputElement).value;
+            setInputValue(input);
+
+          }}>Add Note</button>
+          <p>{inputValue}</p>
+          
+        
+      </div>
       <Footer />
     </>
   )
-
 }
 export default App;
